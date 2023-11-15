0dc46c3e36a6711195f3ead436bb3875cf7c14d4
MarcoPicca <il96marco@gmail.com>
2023-11-15 23:18:51 +0100

    email completata

--- MAIL/js/script.js
+++ MAIL/js/script.js
@@ -1,4 +1,4 @@
-let userEmail = document.getElementById('userEmail');
+let userEmail = document.getElementById('user');
 let button = document.getElementById('button');
 
 
@@ -13,25 +13,31 @@ button.addEventListener('click', function () {
         'pazzoemail@gmail.com',
         'pozzoemail@gmail.com',
         'tazzaemail@gmail.com',
-        'panzaemail@gmail.com',
-        'mezzaemail@gmail.com',
-        'mozzaemail@gmail.com',
-        'cozzaemail@gmail.com',
-        'pizzaemail@gmail.com',
-        'puzzaemail@gmail.com',
-        'pezzaemail@gmail.com',
-        'razzoemail@gmail.com',
-        'razziemail@gmail.com',
-        'pazziemail@gmail.com',
     ];
 
+    // su stackoverflow ho trovato quest idea
+    //  di dare una condizione iniziale (in questo caso falso) ad un elemento che,
+    //  posto in condizione vera quando il valore dell'utente è uguale ad un elemento dell'array,
+    //  fuori dal ciclo può essere richiamato per generare la regola degli alert. 
+    //  Prima di questo metodo ho trovato includes, che comunque non funzionava, ma sembrava più semplice e veloce.
+    //  Non capisco perchè se metto else dopo l'if iniziale non legge le condizioni in funzione della loro risoluzione.
+    // Ps. non ho capito ancora come stampare con innerHTML
+     
 
-for (let i = 0; i < emailList.length; i++){
-    if (!userEmail != emailList[i]){
-        alert('yes');
-    } else {
-        alert('no');
-    }
+
+    let speroSiaCosì = false;
+
+    for (let i = 0; i < emailList.length; i++){
+            if ( userEmail.value === emailList[i] ){
+                speroSiaCosì = true
+                break;
+            }
+        }
+        if (speroSiaCosì) {
+            alert('yes');
+        } else {
+            alert('no');
+        }
+
 }
-
-});
+);
